271ee176a739f911af1e06f867ca9c4eeb395130
HuyNgoc-2011 <hongochuy2@dtu.edu.vn>
2025-08-21 09:22:52 +0700

    addTodo_version1

---
+++ src/app/_components/ShowTodos/ShowTodos.tsx
@@ -0,0 +1,12 @@
+const todoShow = () => {
+    return (
+        <>
+            <h2>Todo List</h2>
+            <div className="todoShow">
+
+            </div>
+        </>
+    )
+}
+
+export default todoShow;
